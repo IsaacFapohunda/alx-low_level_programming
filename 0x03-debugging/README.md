0baa84dae6e3b0727dbfb227fd1962bedfdd53ad
Isaac Feppy <isaacfeppy@gmail.com>
2022-03-18 11:30:37 +0100

    code

--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -1,1 +1,1 @@
-Based on the main.c file above, create a file named 0-main.c. The file must test that the function positive_or_negative() gives the correct output when given a case of 0
+Create a file named 0-main.c. The file must test that the function positive_or_negative() gives the correct output when given a case of 0
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,17 +1,14 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
+#include <stdio.h>
 
-int main(void)
-{
-        int i;
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
 
-        i = 0;
-        positive_or_negative(i);
+void print_remaining_days(int month, int day, int year);
 
-        return (0);
-}
+int convert_day(int month, int day);
+
+#endif /* MAIN_H */
 
